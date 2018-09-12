c2162e48039931da70457f04a1a37c9e9126194a
coronary <cjdekalb@Gmail.com>
2018-09-11 23:18:18 -0500

    Worksheet1 Stuff

---
+++ worksheetFiles/WkSheet1/ConceptsJavaTest.java
@@ -0,0 +1,14 @@
+public class ConceptsJavaTest{ 
+	public static void main(String[] args){
+		int a, b;
+		a = 15;
+		b = 10;
+	//	if(a>b){
+		//	System.out.println("a is greater than b.");
+		//} else {
+			//System.out.println("b is greater than a.");
+		//}
+		int c = (System.out.println("test"), 5);
+		System.out.println(c);
+	}
+}
